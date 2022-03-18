b7f9b0a8f75caf916991817b73af003f52a01bf4
sharifrafid <sharifrafidurrahman@gmail.com>
2022-03-19 03:55:23 +0600

    Moved Stylesheet To _document.js Link

--- pages/_document.js
+++ pages/_document.js
@@ -4,20 +4,24 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                 {/* NOTE: All fonts are loaded for development purposes only.  */}
-                 {/* GOOGLE FONTS  */}
+                {/* NOTE: All fonts are loaded for development purposes only.  */}
+                {/* GOOGLE FONTS  */}
 
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 
-                 {/* Roboto  */}
+                {/* Roboto  */}
                 <link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
                 />
-                 {/* Rubik  */}
+                {/* Rubik  */}
                 <link
                     href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="/assets/css/base.css"
                     rel="stylesheet"
                 />
             </Head>
